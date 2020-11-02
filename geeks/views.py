6200e553383fe405236840acd686173744bc19bd
eab64 <eldos9795@gmail.com>
2020-11-02 18:07:56 +0600

    empty BINs list bug fixed

--- geeks/views.py
+++ geeks/views.py
@@ -20,34 +20,27 @@ def filter(image):
     blurred_jelly = image.filter(ImageFilter.SHARPEN)
     return blurred_jelly
 
-def thrash(img):
+def thresholder(img):
     ret1,th1 = cv2.threshold(img,150,255,cv2.THRESH_BINARY)
     return th1
 
 def date_finder(text):#Проделываем тоже самое
     for i in text:
-        date_re = r'\d\d\.\d\d\.\d\d'
+        date_re = r'[0-3]\d[.|:|-|/][0-1]\d[.|:|-|/]20'
         date = re.findall(date_re, i)
         if len(date) != 0:
             print(date)
             return date[0]
+            # break
 
 def summa_finder(text):
-    total_re = r'\ИТОГ\W\W+\d+'#Ищем по слову ИТОГ
-    total1_re = r'\итог\W\W+\d+'
-    totals_list = []
+    total_re = r"(итог|ИТОГ|Итог|Итого|Итого:|ИТОГО|итого|.ТОГ|.Тог|ОГ|ог)(\W+\d{3,})"#Ищем по слову ИТОГ
     for i in reversed(text):
         total = re.findall(total_re, i)
-        totals_list.append(total)
-    for i in reversed(text):
-        total1 = re.findall(total1_re, i)
-        totals_list.append(total1)
-    for i in totals_list:
-        if len(i)!=0:
-            print(i)
-            XX = ([re.sub('\D+', '', i) for i in i])
-            print(XX)
-            return int(XX[0])#Возвращает все цифры с суммы
+        if len(total)!=0:
+            XX = ([re.sub('\D+', '', total[0][1])])
+            return int(XX[0])
+            break
 
 def bin_finder(text):#Проделываем тоже самое
     BINs_list = []
@@ -60,6 +53,16 @@ def bin_finder(text):#Проделываем тоже самое
     print(list(set(BINs_list)))
     return list(set(BINs_list))#Убрал new_binslist == list(set(BINs_list))
 
+def adress_finder(text):
+    for i in text:
+        date_re = r'DOSTYK|dostyk|Dostyk|ДОСТЫК|достык|PLAZA|plaza|Жолдасбекова|САМАЛ|самал|Самал|Медеус|МЕДЕУС|Фараб|ФАРАБ|фараб|Аль|аль|АЛЬ'
+        # date_re = r'MEGA'
+        date = re.findall(date_re, i)
+        if len(date) != 0:
+            return True
+            break
+        else:
+            return False
 
 custom_config = r'--psm 6 --oem 3'
 
@@ -67,69 +70,58 @@ custom_config = r'--psm 6 --oem 3'
 def index(request):
     data = {}
     texts_list = []
-    if request.method == "POST":
+    if request.method == "POST":#Если запрос POST
         form = GeeksForm(request.POST, request.FILES)
         if form.is_valid():
+            print('ПРИНЯЛ ФОТКУ')
             img = form.cleaned_data.get("image")
-            imgf = Image.open(img)
-            imcv = cv2.cvtColor(numpy.asarray(imgf), cv2.COLOR_RGB2GRAY)
-            texts_list.append(tess.image_to_string(filter(imgf), lang='rus+eng', config=custom_config))  # filt+psm
-            texts_list.append(tess.image_to_string(imgf, lang='rus+eng'))  # default
-            texts_list.append(tess.image_to_string(imgf, lang='rus+eng', config=custom_config))  # def+psm
-            texts_list.append(tess.image_to_string(filter(imgf), lang='rus+eng'))  # filtered
-            texts_list.append(tess.image_to_string(thrash(imcv), lang='eng+rus', config=custom_config))  # threshold
-            for i in bin_finder(texts_list):
-                print(i)
-                #Цикл для проверки всех БИНов
-                response = requests.get(
-                    'https://api.smartplaza.kz/partners/api/brand/bin?bin=' + str(i))
-                data1 = response.json()
-                print("Data1 " + str(data1))
-                for key in data1:
-                    if key == 'city':
-                        data = {'date': date_finder(texts_list), 'brandId': int((data1['brand']['brandId'])), 'sum': summa_finder(texts_list), 'Bin': int(i)}
-                        print("Data " + str(data))
-                        return HttpResponse(json.dumps(data), content_type="application/json")
-            datas = {'date': date_finder(texts_list), 'brandId': 'не найден', 'sum': summa_finder(texts_list), 'Bin': 'не найден'}
-            return HttpResponse(json.dumps(datas), content_type="application/json")
-
+            img_pillow = Image.open(img)
+            img_opencv = cv2.cvtColor(numpy.asarray(img_pillow), cv2.COLOR_RGB2GRAY)
+            texts_list.append(tess.image_to_string(filter(img_pillow), lang='rus+eng', config=custom_config))  # filt+psm
+            texts_list.append(tess.image_to_string(img_pillow, lang='rus+eng'))  # default
+            texts_list.append(tess.image_to_string(img_pillow, lang='rus+eng', config=custom_config))  # def+psm
+            texts_list.append(tess.image_to_string(filter(img_pillow), lang='rus+eng'))  # filtered
+            texts_list.append(tess.image_to_string(thresholder(img_opencv), lang='rus+eng', config=custom_config))  # threshold
+            if adress_finder(texts_list) is True:#Если ТРЦ подходит по названию
+                print('НАЗВАНИЕ ТРЦ ВИДИТ')
+                if len(bin_finder(texts_list))!=0:#Если список БИНОВ не пустой
+                    print('Список бинов не пустой')
+                    for i in bin_finder(texts_list):
+                        response = requests.get(
+                            'https://api.smartplaza.kz/partners/api/brand/bin?bin=' + str(i))
+                        data1 = response.json()#Либо есть данные либо нет(БИНа не существует)
+                        for key in data1:
+                            if key == 'city':#Бренд найден
+                                print('БРЕНД НАЙДЕН')
+                                data = {'date': date_finder(texts_list), 'brandId': int((data1['brand']['brandId'])),
+                                        'sum': summa_finder(texts_list), 'Bin': int(i)}
+                                return HttpResponse(json.dumps(data), content_type="application/json")
+                    data = {'date': date_finder(texts_list), 'brandId': 'не найден',
+                            'sum': summa_finder(texts_list), 'Bin': 'не найден'}
+                    return HttpResponse(json.dumps(data), content_type="application/json")#Бренд не найден
+                else:
+                    print('СПИСОК БИНОВ ПУСТОЙ')
+                    data = {'date': date_finder(texts_list), 'brandId': 'не найден',
+                            'sum': summa_finder(texts_list), 'Bin': 'не найден'}
+                    return HttpResponse(json.dumps(data), content_type="application/json")
+            else:
+                print('НАЗВАНИЕ ТРЦ НЕ ВИДИТ')
     else:
         return HttpResponse(json.dumps("Method not allowed."),
                             content_type="application/json")
+    print('Вернул пустой список')
     return HttpResponse(json.dumps(data),
                         content_type="application/json")
 
 
+
+# elif form.is_valid() and adress_finder(texts_list) is False:
+#     return HttpResponse(json.dumps("Method not allowed."),
+#                         content_type="application/json")
+# else:
+#     data = {'date': date_finder(texts_list), 'brandId': 'не найден', 'sum': summa_finder(texts_list), 'Bin': 'не найден'}
+#     return HttpResponse(json.dumps(data), content_type="application/json")
 # class CheckView(APIView):#Это должно быть в конце и после того как задаются все данные атрибутам выводит все обьекты
 #     def get(self, request):
 #         Checks = Check.objects.all()
 #         return Response({"Checks": Checks})
-
-
-# def bin_finder(text):
-#     Bin_re = r'\d{12}'
-#     Bin = re.findall(Bin_re, text)
-#     return Bin
-
-# def total_finder(text):
-#     some_list = []
-#     total_re = r'\ИТОГ\s\W+\w+\.\w+'  # Ищем по слову ИТОГ
-#     total1_re = r'\итог\s\W+\w+\.\w+'  # Ищем по слову итог
-#     max_total = r'\d+\.'
-#     total = re.findall(total_re, text)
-#     total1 = re.findall(total1_re, text)
-#     itog = re.findall(max_total, text)
-#     if len(total) == 1:  # Условия для отбора
-#         return total
-#     elif len(total1) == 1:
-#         return total1
-#     if len(total) == 0 and len(total1) == 0 and len(itog) != 0:
-#         for i in itog:
-#             i = int(i[:-1])#Приводим в нужный тип, все цифры из списка
-#             some_list.append(i)#Закидываем их в новый список
-#         return max(some_list)
-
-# def date_finder(text):
-#     date_re = r'\d\d\.\d\d\.\d\d'
-#     date = re.findall(date_re, text)
-#     return date
